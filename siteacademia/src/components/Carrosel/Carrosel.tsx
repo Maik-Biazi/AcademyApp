8f26d98f2c53e7ee69ee26cee48309d181288826
Maik-Biazi <71233006+Maik-Biazi@users.noreply.github.com>
2023-01-24 19:52:02 -0300

    24/01/2023

--- siteacademia/src/components/Carrosel/Carrosel.tsx
+++ siteacademia/src/components/Carrosel/Carrosel.tsx
@@ -1,52 +1,58 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import './carrosel.css'
+import "./carrosel.css";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Pagination, Navigation } from "swiper";
 import { Container } from "react-bootstrap";
 
-
 export const Carrosel: React.FC = () => {
-
-    return (
-        <Container fluid>
-            <Swiper
-                slidesPerView={4}
-                spaceBetween={50}
-                slidesPerGroup={2}
-                loop={true}
-                loopFillGroupWithBlank={true}
-                pagination={{
-                    clickable: true,
-                }}
-                navigation={true}
-                modules={[Pagination, Navigation]}
-                className="mySwiper"
-            >
-                <SwiperSlide className="bgImagem1" >
-                          <h1>Isso é um teste</h1>
-                </SwiperSlide>
-                <SwiperSlide className="bgImagem2" >
-                          <h1>Isso é um teste</h1>
-                </SwiperSlide>
-                <SwiperSlide className="bgImagem3">
-                          <h1>Isso é um teste</h1>
-                </SwiperSlide>
-                <SwiperSlide>
-                          <h1>Isso é um teste</h1>
-                </SwiperSlide>
-                <SwiperSlide>
-                          <h1>Isso é um teste</h1>
-                </SwiperSlide>
-                <SwiperSlide>
-                          <h1>Isso é um teste</h1>
-                </SwiperSlide>
-
-
-            </Swiper>
-
-
-        </Container>
-    )
-}
+  return (
+    <Container fluid>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={50}
+        slidesPerGroup={2}
+        loop={true}
+        loopFillGroupWithBlank={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide className="bgImagem1">
+          <div className="carrosel-title">
+            <div>
+              <h3>Circuito funcional</h3>
+            </div>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Molestiae, eaque! Accusamus beatae, eos tenetur ab consequatur
+                laudantium, repellendus nobis ducimus magnam ex nam at aliquam
+                harum iusto quas odio fuga.
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="bgImagem2">
+          <h3>Isso é um teste</h3>
+        </SwiperSlide>
+        <SwiperSlide className="bgImagem3">
+          <h3>Isso é um teste</h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <h3>Isso é um teste</h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <h3>Isso é um teste</h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <h3>Isso é um teste</h3>
+        </SwiperSlide>
+      </Swiper>
+    </Container>
+  );
+};
